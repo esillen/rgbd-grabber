d7300cc9f99e6bb6671d7d63930705d996641fe9
esillen <esillen@kth.se>
2016-02-12 13:49:01 +0100

    fixed stupid bug

--- samples/UEyeCapture.cpp
+++ samples/UEyeCapture.cpp
@@ -23,6 +23,19 @@ using namespace rgbd;
 int main(int argc, char *argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
+
+    std::shared_ptr<UEye> original1;
+    std::shared_ptr<UEye> original2;
+    std::shared_ptr<UEye> original3;
+    std::shared_ptr<UEye> original4;
+
+
+    cv::Mat raw1;
+    cv::Mat raw2;
+    cv::Mat raw3;
+    cv::Mat raw4;
+
+
     bool cam1 = true;
     bool cam2 = true;
     bool cam3 = true;
@@ -36,30 +49,30 @@ int main(int argc, char *argv[]) {
 //    std::shared_ptr<rgbd::ColorRotator> camera(
 //            new rgbd::ColorRotator(calibrated, 90));
     if(cam1){
-        std::shared_ptr<UEye> original1(new UEye(1, "data/ueye-conf.ini"));
+        original1.reset(new UEye(1, "data/ueye-conf.ini"));
         original1->start();
-        cv::Mat raw1 = cv::Mat::zeros(original1->colorSize(), CV_8UC1);
+        raw1 = cv::Mat::zeros(original1->colorSize(), CV_8UC1);
         cv::namedWindow("Raw1", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
     }
 
     if(cam2){
-        std::shared_ptr<UEye> original2(new UEye(2, "data/ueye-conf.ini"));
+        original2(new UEye(2, "data/ueye-conf.ini"));
         original2->start();
-        cv::Mat raw2 = cv::Mat::zeros(original2->colorSize(), CV_8UC1);
+        raw2 = cv::Mat::zeros(original2->colorSize(), CV_8UC1);
         cv::namedWindow("Raw2", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
     }
 
     if(cam3){
-        std::shared_ptr<UEye> original3(new UEye(3, "data/ueye-conf.ini"));
+        original3(new UEye(3, "data/ueye-conf.ini"));
         original3->start();
-        cv::Mat raw3 = cv::Mat::zeros(original3->colorSize(), CV_8UC1);
+        raw3 = cv::Mat::zeros(original3->colorSize(), CV_8UC1);
         cv::namedWindow("Raw3", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
     }
 
     if(cam4){
-        std::shared_ptr<UEye> original4(new UEye(4, "data/ueye-conf.ini"));
+        original4(new UEye(4, "data/ueye-conf.ini"));
         original4->start();
-        cv::Mat raw4 = cv::Mat::zeros(original4->colorSize(), CV_8UC1);
+        raw4 = cv::Mat::zeros(original4->colorSize(), CV_8UC1);
         cv::namedWindow("Raw4", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
     }
     
