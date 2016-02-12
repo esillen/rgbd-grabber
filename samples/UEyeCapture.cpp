2deb31a38cac26a120cc77a449e9681448b56188
esillen <esillen@kth.se>
2016-02-12 13:31:28 +0100

    enklare att testa

--- samples/UEyeCapture.cpp
+++ samples/UEyeCapture.cpp
@@ -23,46 +23,62 @@ using namespace rgbd;
 int main(int argc, char *argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    std::shared_ptr<UEye> original1(new UEye(1, "data/ueye-conf.ini"));
-    std::shared_ptr<UEye> original2(new UEye(2, "data/ueye-conf.ini"));
-    std::shared_ptr<UEye> original3(new UEye(3, "data/ueye-conf.ini"));
-    std::shared_ptr<UEye> original4(new UEye(4, "data/ueye-conf.ini"));
+    bool cam1 = true;
+    bool cam2 = true;
+    bool cam3 = true;
+    bool cam4 = true;
+
+  
     //std::shared_ptr<rgbd::DistortionCalibrator> undistorted(
       //      new rgbd::DistortionCalibrator(original, FLAGS_intrinsics));
     //std::shared_ptr<rgbd::ColorCalibrator> camera(
       //      new rgbd::ColorCalibrator(undistorted));
 //    std::shared_ptr<rgbd::ColorRotator> camera(
 //            new rgbd::ColorRotator(calibrated, 90));
-    original1->start();
-    original2->start();
-    original3->start();
-    original4->start();
-    
-    cv::Mat raw1 = cv::Mat::zeros(original1->colorSize(), CV_8UC1);
-    cv::Mat raw2 = cv::Mat::zeros(original2->colorSize(), CV_8UC1);
-    cv::Mat raw3 = cv::Mat::zeros(original3->colorSize(), CV_8UC1);
-    cv::Mat raw4 = cv::Mat::zeros(original4->colorSize(), CV_8UC1);
+    if(cam1){
+        std::shared_ptr<UEye> original1(new UEye(1, "data/ueye-conf.ini"));
+        original1->start();
+        cv::Mat raw1 = cv::Mat::zeros(original1->colorSize(), CV_8UC1);
+        cv::namedWindow("Raw1", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+    }
 
+    if(cam2){
+        std::shared_ptr<UEye> original2(new UEye(2, "data/ueye-conf.ini"));
+        original2->start();
+        cv::Mat raw2 = cv::Mat::zeros(original2->colorSize(), CV_8UC1);
+        cv::namedWindow("Raw2", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+    }
+
+    if(cam3){
+        std::shared_ptr<UEye> original3(new UEye(3, "data/ueye-conf.ini"));
+        original3->start();
+        cv::Mat raw3 = cv::Mat::zeros(original3->colorSize(), CV_8UC1);
+        cv::namedWindow("Raw3", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+    }
+
+    if(cam4){
+        std::shared_ptr<UEye> original4(new UEye(4, "data/ueye-conf.ini"));
+        original4->start();
+        cv::Mat raw4 = cv::Mat::zeros(original4->colorSize(), CV_8UC1);
+        cv::namedWindow("Raw4", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+    }
     
-    cv::namedWindow("Raw1", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-    cv::namedWindow("Raw2", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-    cv::namedWindow("Raw3", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-    cv::namedWindow("Raw4", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+
 
     int key = 0;
 
     while ((key = cv::waitKey(10)) != 0x1b) {
         
-        original1->captureColor(raw1);
-        original2->captureColor(raw2);
-        original3->captureColor(raw3);
-        original4->captureColor(raw4);
+        if(cam1) original1->captureColor(raw1);
+        if(cam2) original2->captureColor(raw2);
+        if(cam3) original3->captureColor(raw3);
+        if(cam4) original4->captureColor(raw4);
 
         
-        cv::imshow("Raw1", raw1);
-        cv::imshow("Raw2", raw2);
-        cv::imshow("Raw3", raw3);
-        cv::imshow("Raw4", raw4);
+        if(cam1) cv::imshow("Raw1", raw1);
+        if(cam2) cv::imshow("Raw2", raw2);
+        if(cam3) cv::imshow("Raw3", raw3);
+        if(cam4) cv::imshow("Raw4", raw4);
     }
 
     return 0;
